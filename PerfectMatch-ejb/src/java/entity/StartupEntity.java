ba36f5b6869b64704764008df19dc5b021ae2cb5
Anthony <e0406854@u.nus.edu>
2021-03-08 19:19:37 +0800

    added JSF framework, StudentEntity, StartupEntity, web.xml

---
+++ PerfectMatch-ejb/src/java/entity/StartupEntity.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Antho
+ */
+@Entity
+public class StartupEntity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long startupId;
+
+    public Long getStartupId() {
+        return startupId;
+    }
+
+    public void setStartupId(Long startupId) {
+        this.startupId = startupId;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (startupId != null ? startupId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the startupId fields are not set
+        if (!(object instanceof StartupEntity)) {
+            return false;
+        }
+        StartupEntity other = (StartupEntity) object;
+        if ((this.startupId == null && other.startupId != null) || (this.startupId != null && !this.startupId.equals(other.startupId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.StartupEntity[ id=" + startupId + " ]";
+    }
+    
+}
---
+++ PerfectMatch-ejb/src/java/entity/StudentEntity.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Antho
+ */
+@Entity
+public class StudentEntity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long studentId;
+
+    public Long getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(Long studentId) {
+        this.studentId = studentId;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (studentId != null ? studentId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the studentId fields are not set
+        if (!(object instanceof StudentEntity)) {
+            return false;
+        }
+        StudentEntity other = (StudentEntity) object;
+        if ((this.studentId == null && other.studentId != null) || (this.studentId != null && !this.studentId.equals(other.studentId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.StudentEntity[ id=" + studentId + " ]";
+    }
+    
+}
