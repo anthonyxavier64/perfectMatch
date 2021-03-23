3b7547d042ffc79dd43d015dce352a1865984e08
PhireHandy <taha.jailani@gmail.com>
2021-03-23 13:25:49 +0800

    Post-initial deployment

--- PerfectMatch-ejb/build/generated-sources/ap-source-output/entity/Project_.java
+++ PerfectMatch-ejb/build/generated-sources/ap-source-output/entity/Project_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.4.v20190115-rNA", date="2021-03-23T12:52:12")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-03-23T13:25:11")
 @StaticMetamodel(Project.class)
 public class Project_ extends Posting_ {
 
--- PerfectMatch-ejb/build/generated-sources/ap-source-output/entity/Startup_.java
+++ PerfectMatch-ejb/build/generated-sources/ap-source-output/entity/Startup_.java
@@ -9,7 +9,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.4.v20190115-rNA", date="2021-03-23T12:52:12")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-03-23T13:25:11")
 @StaticMetamodel(Startup.class)
 public class Startup_ { 
 
