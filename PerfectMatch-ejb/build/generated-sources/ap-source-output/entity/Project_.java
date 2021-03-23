ba942549278f66e88618287ba5fa65e238360018
Anthony Xavier <e0406854@u.nus.edu>
2021-03-23 03:54:55 +0100

    pushed again

---
+++ PerfectMatch-ejb/build/generated-sources/ap-source-output/entity/Project_.java
@@ -0,0 +1,23 @@
+package entity;
+
+import enumeration.Industry;
+import java.util.Date;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-03-23T00:02:27")
+@StaticMetamodel(Project.class)
+public class Project_ extends Posting_ {
+
+    public static volatile SingularAttribute<Project, String> projectDescription;
+    public static volatile SingularAttribute<Project, Double> compensation;
+    public static volatile SingularAttribute<Project, Date> earliestStartDate;
+    public static volatile SingularAttribute<Project, Date> latestStartDate;
+    public static volatile SingularAttribute<Project, Industry> industry;
+    public static volatile SingularAttribute<Project, String[]> requiredSkills;
+    public static volatile SingularAttribute<Project, String> projectSpecialisation;
+    public static volatile SingularAttribute<Project, String> projectTitle;
+    public static volatile SingularAttribute<Project, Boolean> isComplete;
+
+}
