f883285c4c01dd88bd20ef196a3209b12c82333f
PhireHandy <taha.jailani@gmail.com>
2021-03-25 15:01:14 +0800

    Implemented index
    ToDo: Create DataInit SB in EJB to populate with Startup entities

--- PerfectMatch-ejb/build/generated-sources/ap-source-output/entity/Offer_.java
+++ PerfectMatch-ejb/build/generated-sources/ap-source-output/entity/Offer_.java
@@ -7,7 +7,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-03-25T14:46:07")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-03-25T15:00:20")
 @StaticMetamodel(Offer.class)
 public class Offer_ { 
 
--- PerfectMatch-ejb/build/generated-sources/ap-source-output/entity/Student_.java
+++ PerfectMatch-ejb/build/generated-sources/ap-source-output/entity/Student_.java
@@ -10,7 +10,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-03-25T14:46:07")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-03-25T15:00:20")
 @StaticMetamodel(Student.class)
 public class Student_ { 
 
