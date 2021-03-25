94a555a0d233a08fb7939a26b2cc1c36a3da18af
PhireHandy <taha.jailani@gmail.com>
2021-03-25 13:40:31 +0800

    Set up login
    Have not set up web filters and listeners

--- PerfectMatch-ejb/build/generated-sources/ap-source-output/entity/Offer_.java
+++ PerfectMatch-ejb/build/generated-sources/ap-source-output/entity/Offer_.java
@@ -7,7 +7,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-03-23T13:25:11")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-03-25T13:22:07")
 @StaticMetamodel(Offer.class)
 public class Offer_ { 
 
--- PerfectMatch-ejb/build/generated-sources/ap-source-output/entity/Startup_.java
+++ PerfectMatch-ejb/build/generated-sources/ap-source-output/entity/Startup_.java
@@ -9,7 +9,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-03-23T13:25:11")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-03-25T13:22:07")
 @StaticMetamodel(Startup.class)
 public class Startup_ { 
 
