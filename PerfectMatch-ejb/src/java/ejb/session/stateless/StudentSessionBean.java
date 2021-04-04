036704f485180f99fe7badc5ad9cbad9f9e092e0
yappeizhen <66234273+yappeizhen@users.noreply.github.com>
2021-04-04 14:57:22 +0800

    hi

--- PerfectMatch-ejb/src/java/ejb/session/stateless/StudentSessionBean.java
+++ PerfectMatch-ejb/src/java/ejb/session/stateless/StudentSessionBean.java
@@ -8,7 +8,7 @@ package ejb.session.stateless;
 import entity.Application;
 import entity.Offer;
 import entity.Payment;
-import entity.Startup;
+import entity.StartUp;
 import entity.Student;
 import java.util.List;
 import java.util.Set;
@@ -119,7 +119,7 @@ public class StudentSessionBean implements StudentSessionBeanLocal {
         return students;
     }
 
-    private String prepareInputDataValidationErrorsMessage(Set<ConstraintViolation<Startup>>constraintViolations)
+    private String prepareInputDataValidationErrorsMessage(Set<ConstraintViolation<StartUp>>constraintViolations)
     {
         String msg = "Input data validation error!:";
             
