a3c1da4e3205fb55a8471e77d0f35ac452a48816
PhireHandy <taha.jailani@gmail.com>
2021-04-05 14:20:51 +0800

    Removed bug of having multiple profile pictures

--- PerfectMatch-war/src/java/jsf/managedbean/ProfilePictureManagedBean.java
+++ PerfectMatch-war/src/java/jsf/managedbean/ProfilePictureManagedBean.java
@@ -41,7 +41,33 @@ public class ProfilePictureManagedBean {
                     + "_ProfilePicture"
                     + fileExtension;
 
-            System.out.println("********** profilePictureManagedBean.handleFileUpload() - Absolute Path: " + event.getFile().getFileName());
+            StartUp currentStartup = (StartUp) FacesContext.getCurrentInstance()
+                    .getExternalContext().getSessionMap().get("currentStartup");
+
+            String profilePicturePath = FacesContext
+                    .getCurrentInstance()
+                    .getExternalContext()
+                    .getInitParameter("alternatedocroot_1")
+                    + System.getProperty("file.separator")
+                    + currentStartup.getStartupId()
+                    + "_ProfilePicture";
+            boolean hasProfilePictureJpg = ((new File(profilePicturePath + ".jpg")).exists());
+
+            boolean hasProfilePictureJpeg = ((new File(profilePicturePath + ".jpeg")).exists());
+
+            boolean hasProfilePicturePng = ((new File(profilePicturePath + ".png")).exists());
+
+            if (hasProfilePictureJpg) {
+                File oldPic = new File(profilePicturePath + ".jpg");
+                oldPic.delete();
+            } else if (hasProfilePictureJpeg) {
+                File oldPic = new File(profilePicturePath + ".jpeg");
+                oldPic.delete();
+            }
+            if (hasProfilePicturePng) {
+                File oldPic = new File(profilePicturePath + ".png");
+                oldPic.delete();
+            }
 
             String newFilePath = FacesContext
                     .getCurrentInstance()
