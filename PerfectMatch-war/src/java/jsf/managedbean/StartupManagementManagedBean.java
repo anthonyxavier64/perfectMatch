ef22660b633d18c499579971ac990caaf4eb3cf7
PhireHandy <taha.jailani@gmail.com>
2021-04-06 13:20:07 +0800

    Revert "Revert Taha-merge"
    This reverts commit 9d2812398de9ba7b35b2af0baff1d4434715a487.

--- PerfectMatch-war/src/java/jsf/managedbean/StartupManagementManagedBean.java
+++ PerfectMatch-war/src/java/jsf/managedbean/StartupManagementManagedBean.java
@@ -13,14 +13,13 @@ import enumeration.Industry;
 import enumeration.StartUpLocation;
 import java.io.File;
 import java.io.Serializable;
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
 import javax.inject.Named;
 import javax.faces.view.ViewScoped;
-import org.primefaces.model.DefaultStreamedContent;
-import org.primefaces.model.StreamedContent;
 import util.exception.CreateNewStartUpException;
 import util.exception.InputDataValidationException;
 
@@ -28,7 +27,7 @@ import util.exception.InputDataValidationException;
  *
  * @author Phire
  */
-@Named(value = "startupManagementManagedBean")
+@Named(value = "startUpManagementManagedBean")
 @ViewScoped
 public class StartupManagementManagedBean implements Serializable {
 
@@ -39,12 +38,16 @@ public class StartupManagementManagedBean implements Serializable {
     @EJB
     private StartUpSessionBeanLocal startUpSessionBean;
 
+    // Used to register a new StartUp
     private StartUp newStartUp;
     private StartUpLocation[] startUpLocations;
     private Industry[] industries;
 
-    private StreamedContent startUpProfilePicture;
-
+    // Used to update StartUp
+    private StartUp currentStartUp;
+    private Industry updateIndustry;
+    private StartUpLocation updateLocation;
+    
     /**
      * Creates a new instance of StartupManagementManagedBean
      */
@@ -53,10 +56,21 @@ public class StartupManagementManagedBean implements Serializable {
         startUpLocations = StartUpLocation.values();
         industries = Industry.values();
     }
+    
+    @PostConstruct
+    public void postConstruct() {
+        currentStartUp = (StartUp) FacesContext.getCurrentInstance()
+                .getExternalContext().getSessionMap().get("currentStartUp");
+        updateIndustry = currentStartUp.getIndustry();
+        updateLocation = currentStartUp.getStartupLocation();
+    }
 
-    public void DynamicImageController() {
+    public void retrieveCurrentStartUpFromContext() {
+    }
+    
+    public void setProfilePic() {
         StartUp currentStartup = (StartUp) FacesContext.getCurrentInstance()
-                .getExternalContext().getSessionMap().get("currentStartup");
+                .getExternalContext().getSessionMap().get("currentStartUp");
 
         String profilePicturePath = FacesContext
                 .getCurrentInstance()
@@ -74,24 +88,21 @@ public class StartupManagementManagedBean implements Serializable {
         if (hasProfilePictureJpg) {
             System.out.println("********** StartupManagementManagedBean.hasProfilePictureJpg");
 
-            startUpProfilePicture = DefaultStreamedContent.builder()
-                    .contentType("image/jpg")
-                    .stream(() -> this.getClass().getResourceAsStream(profilePicturePath + ".jpg"))
-                    .build();
+            String startUpProfilePicture = currentStartup.getStartupId() + "_ProfilePicture.jpg";
+            FacesContext.getCurrentInstance().getExternalContext().getSessionMap()
+                    .put("startUpProfilePicture", startUpProfilePicture);
         } else if (hasProfilePictureJpeg) {
             System.out.println("********** StartupManagementManagedBean.hasProfilePictureJpeg");
-
-            startUpProfilePicture = DefaultStreamedContent.builder()
-                    .contentType("image/jpeg")
-                    .stream(() -> this.getClass().getResourceAsStream(profilePicturePath + ".jpeg"))
-                    .build();
+            
+            String startUpProfilePicture = currentStartup.getStartupId() + "_ProfilePicture.jpeg";
+            FacesContext.getCurrentInstance().getExternalContext().getSessionMap()
+                    .put("startUpProfilePicture", startUpProfilePicture);
         } else if (hasProfilePicturePng) {
             System.out.println("********** StartupManagementManagedBean.hasProfilePicturePng");
-
-            startUpProfilePicture = DefaultStreamedContent.builder()
-                    .contentType("image/png")
-                    .stream(() -> this.getClass().getResourceAsStream("/" + profilePicturePath + ".png"))
-                    .build();
+            
+            String startUpProfilePicture = currentStartup.getStartupId() + "_ProfilePicture.png";
+            FacesContext.getCurrentInstance().getExternalContext().getSessionMap()
+                    .put("startUpProfilePicture", startUpProfilePicture);
         }
     }
 
@@ -102,20 +113,47 @@ public class StartupManagementManagedBean implements Serializable {
                     .addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO,
                             "New StartUp registered (StartUp ID: " + startup.getStartupId() + ")",
                             null));
+            newStartUp = new StartUp();
         } catch (InputDataValidationException | CreateNewStartUpException ex) {
             FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "An error has occurred while registering the new StartUp: " + ex.getMessage(), null));
         }
     }
 
-    public StreamedContent getStartUpProfilePicture() {
-        return startUpProfilePicture;
+    public void updateStartUp(ActionEvent event) {
+        currentStartUp.setIndustry(updateIndustry);
+        currentStartUp.setStartupLocation(updateLocation);
+        startUpSessionBean.updateStartUp(currentStartUp);
+        
+        FacesContext.getCurrentInstance().getExternalContext().getSessionMap()
+                    .put("currentStartUp", currentStartUp);
     }
 
-    public void setStartUpProfilePicture(StreamedContent startUpProfilePicture) {
-        this.startUpProfilePicture = startUpProfilePicture;
+    public Industry getUpdateIndustry() {
+        return updateIndustry;
     }
 
+    public void setUpdateIndustry(Industry updateIndustry) {
+        this.updateIndustry = updateIndustry;
+    }
+
+    public StartUpLocation getUpdateLocation() {
+        return updateLocation;
+    }
+
+    public void setUpdateLocation(StartUpLocation updateLocation) {
+        this.updateLocation = updateLocation;
+    }
     
+    
+    
+    public StartUp getCurrentStartUp() {
+        return currentStartUp;
+    }
+
+    public void setCurrentStartUp(StartUp currentStartUp) {
+        this.currentStartUp = currentStartUp;
+    }
+
     
     public StartUp getNewStartUp() {
         return newStartUp;
